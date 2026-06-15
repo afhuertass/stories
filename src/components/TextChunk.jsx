0d2deb9dd1c9a115452eacb1d3ddfd949eac7413
afhuertass <afhuertass@unal.edu.co>
2026-06-15 06:27:20 +0300

    Indent

--- src/components/TextChunk.jsx
+++ src/components/TextChunk.jsx
@@ -31,7 +31,7 @@ export default function TextChunk({ chunk }) {
       {chunk.content.map((paragraph, index) => (
         <motion.div
           key={index}
-          className="mb-2"
+          className="mb-2 indent-8"
           initial={{ opacity: 0, y: 15 }}
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{ once: true, amount: 0.1 }}
